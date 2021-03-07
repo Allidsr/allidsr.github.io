66d9394bfa26a7c4626bc5793909a60c803d40f8
allidsr <agu20010@byui.edu>
2021-03-06 20:41:59 -0600

    update

--- lesson09/js/towns.js
+++ lesson09/js/towns.js
@@ -8,6 +8,7 @@ const requestURL =
   .then(function (jsonObject) {
     const towns = jsonObject["towns"];
     for (let i = 0; i < towns.length; i++) {
+      if ((i == 1) || (i == 5) || (i == 6)) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let m = document.createElement("p")
@@ -21,9 +22,15 @@ const requestURL =
       year.textContent = "Year Founded: " + towns[i].yearFounded;
       pop.textContent = "Population: " + towns[i].currentPopulation;
       arf.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-      image.setAttribute("src", towns[i].photo);
-      image.setAttribute("alt", towns[i].name);
-
+      if (i == 1) {
+        image.setAttribute("src", "images/fish-haven.jpg");
+        image.setAttribute("alt", towns[i].name);}
+      else if (i == 5) {
+      image.setAttribute("src", "images/preston.jpg");
+      image.setAttribute("alt", towns[i].name); }
+      else if (i == 6) {
+      image.setAttribute("src", "images/soda-springs.jpg");
+      image.setAttribute("alt", towns[i].name); }
 
       card.appendChild(h2);
       card.appendChild(m);
@@ -34,5 +41,5 @@ const requestURL =
       card.setAttribute("class", "card");
 
       document.querySelector("div.cards").appendChild(card);
-    }
+    }}
   });
